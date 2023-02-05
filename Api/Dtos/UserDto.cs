87f611a78fd45a220c05e77542e4d1ce1ec1d264
Subrata Mukherjee <subrotomukherjee8@gmail.com>
2023-02-06 00:15:05 +0530

    chat app
    Basic Chat application

---
+++ Api/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dtos
+{
+    public class UserDto
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "NName must be at least (2), and maximum (1) characters")]
+        public string Name { get; set; }
+
+    }
+}
